23082babf4429cf8ae8fdc197b49d4d5932ed485
Aradhana-ags <104771380+Aradhana-ags@users.noreply.github.com>
2022-05-02 21:05:18 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,200 @@
+namespace SpriteKind {
+    export const tree = SpriteKind.create()
+}
+let projectile: Sprite = null
+let mySprite: Sprite = null
+let mySprite3: Sprite = null
+let mySprite5: Sprite = null
+let mySprite6: Sprite = null
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 6 6 6 6 5 . . . 
+        . . . . . 5 6 6 6 6 6 6 6 . . . 
+        . . . . . 6 6 6 6 6 6 6 6 . . . 
+        . . 5 6 6 6 6 6 6 6 6 6 5 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 200, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -150
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    mySprite3.destroy()
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(4, 500)
+})
+game.onUpdate(function () {
+    mySprite.setImage(img`
+        . . . f f f f f . . . . . 
+        . f f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f c f f f c f f . . 
+        f c f f c c f f f c c f f 
+        f c c f f f f e f f f f f 
+        f f f f f f f e e f f f . 
+        f f e e f b f e e f f . . 
+        . f e 4 e 1 f 4 4 f . . . 
+        . f f f e 4 4 4 4 f . . . 
+        . . f e e e e e f f . . . 
+        . . e 4 4 e 7 7 7 f . . . 
+        . . e 4 4 e 7 7 7 f . . . 
+        . . f e e f 6 6 6 f . . . 
+        . . . f f f f f f . . . . 
+        . . . . f f f . . . . . . 
+        `)
+    if (mySprite.vy < 0) {
+        mySprite.setImage(img`
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f . . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f . . . 
+            . f f f e 4 4 4 4 f . . . 
+            . . f e e e e e f f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . f e e f 6 6 6 f . . . 
+            . . . f f f f f f . . . . 
+            . . . . f f f . . . . . . 
+            . . . . . . . . . . . . . 
+            `)
+    } else if (mySprite.vy > 0) {
+        mySprite.setImage(img`
+            . . . f f f f f . . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f . . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f . . . 
+            . f f f e 4 4 4 4 f . . . 
+            . . f e e e e e f f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . f e e f 6 6 6 f . . . 
+            . . . f f f f f f . . . . 
+            . . . . f f f . . . . . . 
+            `)
+    } else if (mySprite.x % 2 == 0) {
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . 
+            . . . f f f f f f . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f f . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f f . . 
+            . f f f e e 4 4 4 f . . . 
+            . . f e 4 4 e e f f . . . 
+            . . f e 4 4 e 7 7 f . . . 
+            . f f f e e f 6 6 f f . . 
+            . f f f f f f f f f f . . 
+            . . f f . . . f f f . . . 
+            `)
+    } else {
+    	
+    }
+    if (mySprite.vx < 0) {
+        mySprite.image.flipX()
+    }
+})
+game.onUpdateInterval(2000, function () {
+    mySprite3 = sprites.create(img`
+        ...............ff.......
+        .............ff2ffff....
+        ............ff2feeeeff..
+        ...........ff22feeeeeff.
+        ...........feeeeffeeeef.
+        ..........fe2222eefffff.
+        ..........f2effff222efff
+        ..........fffeeeffffffff
+        ..........fee44fbe44efef
+        ...........feddfb4d4eef.
+        ..........c.eeddd4eeef..
+        ....ccccccceddee2222f...
+        .....dddddcedd44e444f...
+        ......ccccc.eeeefffff...
+        ..........c...ffffffff..
+        ...............ff..fff..
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    mySprite3.setPosition(258, 125)
+    mySprite5 = sprites.create(img`
+        ...............ff.......
+        .............ff2ffff....
+        ............ff2feeeeff..
+        ...........ff22feeeeeff.
+        ...........feeeeffeeeef.
+        ..........fe2222eefffff.
+        ..........f2effff222efff
+        ..........fffeeeffffffff
+        ..........fee44fbe44efef
+        ...........feddfb4d4eef.
+        ..........c.eeddd4eeef..
+        ....ccccccceddee2222f...
+        .....dddddcedd44e444f...
+        ......ccccc.eeeefffff...
+        ..........c...ffffffff..
+        ...............ff..fff..
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    mySprite5.setPosition(388, 125)
+    mySprite6 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
